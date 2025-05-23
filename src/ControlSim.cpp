e05feaf1b46448a2cfa14d041d8b58faf1e80bee
Sámuel Varga <varga.samu@gmail.com>
2025-05-23 03:00:09 +0200

    update main

--- src/ControlSim.cpp
+++ src/ControlSim.cpp
@@ -14,112 +14,36 @@ double Mod(double a, double b) {
 }
 
 int main() {
-	//// Create a window
-	//int videoWidth = 1600, videoHeight = 900;
-	//sf::RenderWindow window(sf::VideoMode(videoWidth, videoHeight), "Vehicle Simulation");
-	//int frameRate = 144;
-	//double dt = 1.0 / frameRate;
-	//window.setFramerateLimit(144);
-	//// Create a vehicle
-	//double zoom = 25.0;
-	//model::Point startPos(videoWidth / 2, videoHeight / 2);
-	////model::Vehicle vehicle(videoWidth /zoom/2, videoHeight/zoom/2,0.0,0.0);
-	//model::Vehicle vehicle(std::make_unique<controller::KeyboardControl>());
 
 	std::array<model::Cone, 18> map({
-		model::Cone(0, 0, 0.3),
-		model::Cone(0, 3, 0.3),
-		model::Cone(5, 0, 0.3),
-		model::Cone(5, 3, 0.3),
-		model::Cone(10, 0, 0.3),
-		model::Cone(10, 3, 0.3),
-		model::Cone(15, 0, 0.3),
-		model::Cone(15, 3, 0.3),
-		model::Cone(20, 0, 0.3),
-		model::Cone(20, 3, 0.3),
-		model::Cone(25, 0, 0.3),
-		model::Cone(25, 3, 0.3),
-		model::Cone(30, 0, 0.3),
-		model::Cone(30, 3, 0.3),
-		model::Cone(35, 0, 0.3),
-		model::Cone(35, 3, 0.3),
-		model::Cone(40, 0, 0.3),
-		model::Cone(40, 3, 0.3),
+		model::Cone(0, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(0, 1.5, 0.15,model::ConeType::RIGHT),
+		model::Cone(5, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(5, 1.5, 0.15,model::ConeType::RIGHT),
+		model::Cone(10, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(10, 1.5, 0.15,model::ConeType::RIGHT),
+		model::Cone(15, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(15, 1.5, 0.15,model::ConeType::RIGHT),
+		model::Cone(20, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(20, 1.5, 0.15,model::ConeType::RIGHT),
+		model::Cone(25, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(25, 1.5, 0.15,model::ConeType::RIGHT),
+		model::Cone(30, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(30, 1.5, 0.15,model::ConeType::RIGHT),
+		model::Cone(35, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(35, 1.5, 0.15,model::ConeType::RIGHT),
+		model::Cone(40, -1.5, 0.15,model::ConeType::LEFT),
+		model::Cone(40, 1.5, 0.15,model::ConeType::RIGHT),
 		});
 
+	//std::array<model::Cone, 0> map({});
+	
 	model::App app(
-		std::make_unique<model::Vehicle>(std::make_unique<controller::KeyboardControl>()),
+		std::make_unique<model::Vehicle>(std::make_unique<controller::KeyboardControl>(),std::make_unique<model::PathPlanner>(40,1.5,1/15.0)),
 		std::make_unique<model::ManualMapReader<std::array<model::Cone, 18>>>(map),
 		std::make_unique<view::AppView>()
 	);
 
 	app.run();
 	
-	//// Main loop
-	//while (window.isOpen()) {
-	//	sf::Event event;
-	//	while (window.pollEvent(event)) {
-	//		if (event.type == sf::Event::Closed)
-	//			window.close();
-
-	//		if (event.type == sf::Event::KeyPressed) {
-	//			if (event.key.code == sf::Keyboard::Escape)
-	//				window.close();
-	//		}
-	//	}
-	//	//// Steering
-	//	//vehicle.steer();
-
-	//	//// Acceleration
-	//	//vehicle.drive();
-
-	//	// Update vehicle state
-	//	vehicle.update(dt);
-	//	// Clear the window
-	//	window.clear(sf::Color::Black);
-	//	//// Draw the vehicle (for demonstration purposes)
-	//	//auto positions = vehicle.getAllPositions();
-	//	//auto orientations = vehicle.getAllOrientations();
-	//	//std::array<sf::Color, 3> shapeColor = { sf::Color::Green, sf::Color::Red,sf::Color::Red };
-	//	//std::array<std::array<double, 2>, 3> shapeSize{};
-	//	//shapeSize[0] = { vehicle.getLength(),vehicle.getWidth() };
-	//	//shapeSize[1] = { vehicle.getWidth() * 0.75,vehicle.getWidth()*1.5 };
-	//	//shapeSize[2] = shapeSize[1];
-	//	//for (int i = 0; i < 3;i++) {
-	//	//	sf::RectangleShape shape(sf::Vector2f(shapeSize[i][0] * zoom, shapeSize[i][1] * zoom));
-	//	//	shape.setOrigin(shapeSize[i][0] * zoom / 2, shapeSize[i][1] * zoom / 2);
-	//	//	model::Point realPos = (positions[i]-positions[1]) * zoom + startPos;
-	//	//	realPos.setX(Mod(realPos.X(), videoWidth));
-	//	//	realPos.setY(Mod(realPos.Y(), videoHeight));
-
-	//	//	shape.setFillColor(shapeColor[i]);
-	//	//	shape.setPosition(realPos.X(), realPos.Y());
-	//	//	shape.setRotation(orientations[i]/M_PI*180);
-	//	//	//std::cout << "Vehicle Position: " << positions[0].X() << ", " << positions[0].Y() << std::endl;
-	//	//	window.draw(shape);
-	//	//}
-	//	// Draw the vehicle
-	//	auto position = vehicle.getPosition();
-
-	//	sf::RectangleShape vehicleShape(sf::Vector2f(vehicle.getLength()*zoom, vehicle.getWidth() *zoom));
-	//	vehicleShape.setOrigin(vehicle.getLength() * zoom / 2, vehicle.getWidth() * zoom /2);
-	//	vehicleShape.setFillColor(sf::Color::Red);
-	//	vehicleShape.setPosition(videoWidth / 2, videoHeight / 2);
-	//	vehicleShape.setRotation(vehicle.getOrientation() / M_PI * 180);
-	//	window.draw(vehicleShape);
-
-	//	// Draw the map
-	//	for (auto& cone : map) {
-	//		sf::CircleShape shape(0.3 * zoom);
-	//		//model::Point realPos = (cone.getPosition() - positions[1] )* zoom + startPos;
-	//		model::Point realPos = (cone.getPosition() - position) * zoom + startPos;
-	//		realPos.setX(Mod(realPos.X(), videoWidth));
-	//		realPos.setY(Mod(realPos.Y(), videoHeight));
-	//		shape.setFillColor(sf::Color::Blue);
-	//		shape.setPosition(realPos.X(), realPos.Y());
-	//		window.draw(shape);
-	//	}
-	//	// Display the contents of the window
-	//	window.display();
-	//}
 }
