88df2f2af0cbbcea023182e9c61a0e8da4de3731
Sámuel Varga <varga.samu@gmail.com>
2025-05-23 02:53:13 +0200

    Add control input struct and hash for std::pair

--- src/model/utils/ModelUtils.cpp
+++ src/model/utils/ModelUtils.cpp
@@ -3,6 +3,7 @@
 #include "ModelUtils.h"
 
 namespace model {
+
 	double clamp(double value, double min, double max) {
 		return std::max(min, std::min(value, max));
 	}
--- src/model/utils/ModelUtils.h
+++ src/model/utils/ModelUtils.h
@@ -1,6 +1,36 @@
 #pragma once
 
 namespace model {
+	struct ControlInput {
+		double targetSpeed;
+		double steeringAngle;
+		ControlInput(double targetSpeed, double steeringAngle) : targetSpeed(targetSpeed), steeringAngle(steeringAngle) {}
+	};
+
     double clamp(double value, double min, double max);
 
+    template <class T>
+    inline void hash_combine(std::size_t& seed, const T& v)
+    {
+        std::hash<T> hasher;
+        seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
+
+
+
+}
+
+namespace std
+{
+	template <typename T1, typename T2>
+    struct hash<pair<T1,T2>>
+    {
+        inline size_t operator()(const pair<T1,T2>& p) const
+        {
+            size_t seed = 0;
+            model::hash_combine(seed, p.first);
+            model::hash_combine(seed, p.second);
+            return seed;
+        }
+    };
 }
