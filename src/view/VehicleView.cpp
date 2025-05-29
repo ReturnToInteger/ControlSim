4edb5e289de4fa279e1f0ded478e5f7c743a3e81
Sámuel Varga <varga.samu@gmail.com>
2025-05-29 03:59:24 +0200

    Add vehicle sprite

--- src/view/VehicleView.cpp
+++ src/view/VehicleView.cpp
@@ -10,15 +10,20 @@ void view::VehicleView::draw(sf::RenderTarget& target, sf::RenderStates states =
 
 	sf::RectangleShape vehicleShape(sf::Vector2f(_vehicle->getLength(),
 		_vehicle->getWidth()));
-	vehicleShape.setOrigin(_vehicle->getLength()/2.0, _vehicle->getWidth()/2.0);
-	vehicleShape.setFillColor(_color);
-	vehicleShape.setPosition(position.X(),position.Y());
-	vehicleShape.setRotation(_vehicle->getOrientation() / M_PI * 180);
 
-	sf::RectangleShape leftWheel(sf::Vector2f(_vehicle->getLength() / 4.0,
-		_vehicle->getWidth() / 4.0));
-
-	target.draw(vehicleShape);
+	sf::Sprite sprite(_texture);
+	//vehicleSprite.setTextureRect(sf::IntRect(
+	//	0, 0,
+	//	static_cast<int>(_vehicle->getWidth()) * 10,
+	//	static_cast<int>(_vehicle->getLength()) * 10
+	//));
+	sf::Vector2u size=_texture.getSize();
+	sprite.setOrigin(size.x/2.0, size.y/2.0);
+	sprite.setRotation(_vehicle->getOrientation() / M_PI * 180+90);
+	sprite.setScale(_vehicle->getWidth() / size.x, _vehicle->getLength() / size.y);
+	//vehicleSprite.setFillColor(_color);
+	sprite.setPosition(position.X(),position.Y());
+	target.draw(sprite);
 }
 
 view::VehicleView::VehicleView()
@@ -27,8 +32,11 @@ view::VehicleView::VehicleView()
 }
 
 view::VehicleView::VehicleView(const model::Vehicle& vehicle, sf::Color color)
-	: ItemView(color), _vehicle(&vehicle) 
+	: ItemView(color), _vehicle(&vehicle)
 {
+	if (!_texture.loadFromFile("car.png"))
+		throw std::ios_base::failure("Image not found");
+;
 	isDetected = true;
 }
 
