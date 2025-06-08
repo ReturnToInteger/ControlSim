5651ca2759b318948ae03bce718f27273daba205
Sámuel Varga <varga.samu@gmail.com>
2025-06-08 12:53:55 +0200

    Move constructor definition

--- src/model/items/Path.cpp
+++ src/model/items/Path.cpp
@@ -1,8 +1,10 @@
 #include "Path.h"
+#include <iostream>
 
 namespace model {
 	Point model::Path::getPosition() const
 	{
 		return Point();
 	}
+	Path::Path(std::vector<Pose> const& path) : _path(path) { if (_path.size() == 0) std::cout << "No path created" << std::endl; }
 }
--- src/model/items/Path.h
+++ src/model/items/Path.h
@@ -3,6 +3,7 @@
 #include "model/utils/Point.h"
 #include "model/utils/Pose.h"
 #include <vector>
+
 //#include <model/pathPlanner/PathNode.h>
 namespace model {
     class Path :
@@ -12,11 +13,12 @@ namespace model {
         // Inherited via Item
         Point getPosition() const override;
         Path() : _path() {}
-        Path(const std::vector<Pose>& path) : _path(path) {}
+        Path(std::vector<Pose> const& path);
+
 
         // Vector wrapper
         //Pose& operator[](size_t index) { return _path[index]; }
-        const Pose& operator[](size_t index) const { return _path[index]; }
+        Pose const& operator[](size_t index) const { return _path[index]; }
         //std::vector<Pose>::iterator begin() { return _path.begin(); }
         //std::vector<Pose>::iterator end() { return _path.end(); }
         std::vector<Pose>::const_iterator begin() const { return _path.begin(); }
