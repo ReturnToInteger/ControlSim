8c758f9ca1186e070dc54e8b84729c8eaf500e47
Sámuel Varga <varga.samu@gmail.com>
2025-06-08 12:52:38 +0200

    Implement InputEventHandler

--- src/controller/App.h
+++ src/controller/App.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <mutex>
 #include "model/items/Cone.h"
+#include "model/eventHandlers/InputEventHandler.h"
+#include "model/eventHandlers/InputEventPublisher.h"
 //#include "model/perception/Perception.h"
 
 
@@ -9,13 +11,25 @@ namespace model {
 	class Vehicle;
 	class Perception;
 	class IMapReader;
+	class VehicleState;
 }
 namespace view {
 	class AppView;
 }
 
 namespace controller {
-    class App
+	using model::InputEvent;
+	struct InputTranslate {
+		void operator()(model::PressedEsc const&) { std::cout << "Pressed ESC." << std::endl; }
+		void operator()(model::PressedLShift const&) {std::cout << "Pressed LShift." << std::endl; }
+		void operator()(model::Scrolled const& i) {std::cout << "zoom: " << i.delta << std::endl; }
+		void operator()(model::ClickedAt const& i) {std::cout << "Clicked at :" << i.x << "; " << i.y << std::endl; }
+		void operator()(model::Resized const&) {std::cout << "Resizing..." << std::endl; }
+		void operator()(model::LostFocus const&) {std::cout << "Lost Focus." << std::endl; }
+		void operator()(model::GainedFocus const&) { std::cout << "Gained Focus." << std::endl; }
+		void operator()(model::None const&) {}
+	};
+    class App : public model::InputEventHandler
     {
     private:
 		std::vector<model::Cone> _cones;
@@ -26,6 +40,12 @@ namespace controller {
 
 		// Need a reader, which will read the map 
 		std::unique_ptr<model::IMapReader> _mapReader;
+		// Should be moved inside path planner
+		void _calcGoal(std::vector<const model::Cone*> const& cones, model::VehicleState const& state);
+
+
+
+
 	public:
 		App(std::unique_ptr<model::Vehicle> vehicle,
 			std::unique_ptr<model::IMapReader> mapReader,
@@ -33,8 +53,13 @@ namespace controller {
 
 		// Need a run method, which will run the game
 		void run();
+
 		~App();
 
 
-    };
+
+		// Inherited via InputEventHandler
+		void handleInputEvent(std::string const& src, model::InputEvent const& e) override;
+
+	};
 }
