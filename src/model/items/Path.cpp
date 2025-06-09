f07002fb472c445a7242740ffa0da2e19c5e32fd
ReturnToInteger <varga.samu@gmail.com>
2025-06-09 02:07:12 +0200

    Path now uses VehicleState instead of Pose for custom control implementation

--- src/model/items/Path.cpp
+++ src/model/items/Path.cpp
@@ -1,10 +1,11 @@
 #include "Path.h"
 #include <iostream>
+#include "model/VehicleState.h"
 
 namespace model {
 	Point model::Path::getPosition() const
 	{
 		return Point();
 	}
-	Path::Path(std::vector<Pose> const& path) : _path(path) { if (_path.size() == 0) std::cout << "No path created" << std::endl; }
+	Path::Path(std::vector<VehicleState> const& path) : _path(path) { if (_path.size() == 0) std::cout << "No path created" << std::endl; }
 }
--- src/model/items/Path.h
+++ src/model/items/Path.h
@@ -1,31 +1,28 @@
 #pragma once
 #include "model/items/Item.h"
 #include "model/utils/Point.h"
-#include "model/utils/Pose.h"
+#include "model/VehicleState.h"
 #include <vector>
 
-//#include <model/pathPlanner/PathNode.h>
-namespace model {
+namespace model {        
+    // Read-only vector<VehicleState> wrapper
     class Path :
         public Item
     {
     public:
         // Inherited via Item
         Point getPosition() const override;
+        Path(std::vector<VehicleState> const& path);
         Path() : _path() {}
-        Path(std::vector<Pose> const& path);
 
 
-        // Vector wrapper
-        //Pose& operator[](size_t index) { return _path[index]; }
-        Pose const& operator[](size_t index) const { return _path[index]; }
-        //std::vector<Pose>::iterator begin() { return _path.begin(); }
-        //std::vector<Pose>::iterator end() { return _path.end(); }
-        std::vector<Pose>::const_iterator begin() const { return _path.begin(); }
-        std::vector<Pose>::const_iterator end() const { return _path.end(); }
+        VehicleState const& operator[](size_t index) const { return _path[index]; }
+        std::vector<VehicleState>::const_iterator begin() const { return _path.begin(); }
+        std::vector<VehicleState>::const_iterator end() const { return _path.end(); }
         size_t size() const { return _path.size(); }
+        VehicleState const& back() const { return _path.back(); }
 
-    private:
-        std::vector<Pose> _path;
+    private:        
+        std::vector<VehicleState> _path;
     };
 }
