907dfdb8049b1e8576548f6ece646f4a332ff017
ReturnToInteger <varga.samu@gmail.com>
2025-06-10 23:58:21 +0200

    Add missing virtual destructor, change input to Path from VehicleState

--- src/model/controllerLogic/IControllerLogic.h
+++ src/model/controllerLogic/IControllerLogic.h
@@ -2,15 +2,13 @@
 #include "model/controllerLogic/ControlCommand.h"
 
 namespace model {
-	namespace pathPlanning {
-		class PathPlanner;
-	}
+	class Path;
 	class VehicleState;
 	class IControllerLogic {
 	public:
 		IControllerLogic() = default;
-		~IControllerLogic() = default;
-		virtual ControlCommand drive(VehicleState const& state, pathPlanning::PathPlanner const& pathPlanner) = 0;
+		virtual ~IControllerLogic() = default;
+		virtual ControlCommand drive(VehicleState const& state, model::Path const& path) = 0;
 
 	};
 }
